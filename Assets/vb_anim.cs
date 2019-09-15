af19fae846ce99b7ccbe4cac338c2c3c8ae53e32
Amit Panthi <47650855+apanth2@users.noreply.github.com>
2019-09-14 19:24:46 -0600

    cs 428
    PROJECT1

---
+++ Assets/vb_anim.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class vb_anim : MonoBehaviour , IVirtualButtonEventHandler
+{
+
+    public GameObject vbBtnObj;
+    public GameObject vendetta;
+    // public Anima cubeAni;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        vbBtnObj = GameObject.Find("Laciebtn");
+        vendetta = GameObject.Find("V");
+        vendetta.SetActive(false);
+        vbBtnObj.GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);
+        
+        
+    }
+
+    public void OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+        vb.gameObject.SetActive(true);
+        Debug.Log("Amit");
+    }
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+        Debug.Log("Released");
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
